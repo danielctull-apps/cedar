e691f4969f92d8b0b1cfafbc297e2a2cee4f05d3
Daniel Tull <dt@danieltull.co.uk>
2020-07-16 18:13:45 +0100

    Set the items as an array rather than adding each item

--- Sources/Cedar/Menu.swift
+++ Sources/Cedar/Menu.swift
@@ -9,24 +9,28 @@ extension NSMenu {
 
         let app = NSMenuItem()
         app.submenu = NSMenu()
-        app.submenu?.addItem(NSMenuItem(title: "Quit \(name)", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        app.submenu?.items = [
+            NSMenuItem(title: "Quit \(name)", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"),
+        ]
 
         let edit = NSMenuItem()
         edit.submenu = NSMenu(title: "Edit")
-        edit.submenu?.addItem(NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"))
-        edit.submenu?.addItem(NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"))
-        edit.submenu?.addItem(NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"))
-        edit.submenu?.addItem(NSMenuItem.separator())
-        edit.submenu?.addItem(NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"))
+        edit.submenu?.items = [
+            NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"),
+            NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"),
+            NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"),
+            NSMenuItem.separator(),
+            NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"),
+        ]
 
         let window = NSMenuItem()
         window.submenu = NSMenu(title: "Window")
-        window.submenu?.addItem(NSMenuItem(title: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w"))
+        window.submenu?.items = [
+            NSMenuItem(title: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w"),
+        ]
 
         let menu = NSMenu(title: "Main Menu")
-        menu.addItem(app)
-        menu.addItem(edit)
-        menu.addItem(window)
+        menu.items = [app, edit, window]
         return menu
     }
 }
